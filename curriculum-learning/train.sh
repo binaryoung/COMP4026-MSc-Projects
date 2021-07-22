b099d7f5bebc031d508ba1e7d9e51172d23548f4
young <me@xiayang.me>
2021-07-22 18:13:04 +0100

    curriculum learning: 优化GPU集群训练脚本逻辑

--- curriculum-learning/train.sh
+++ curriculum-learning/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p cs -A cs -q csug
-#SBATCH -c8 --mem=16g
+#SBATCH -c4 --mem=8g
 #SBATCH --gres gpu:1
 module load nvidia/cuda-11.0
 module load nvidia/cudnn-v8.0.180-forcuda11.0
-python ./ppo.py
+python $1
