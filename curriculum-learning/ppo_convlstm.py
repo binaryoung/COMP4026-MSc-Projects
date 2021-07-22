8830c93545b7a7eed76839732c547a472c468bf9
young <me@xiayang.me>
2021-07-22 18:12:02 +0100

    curriculum learning: 初步完成PPO ConvLSTM算法

--- curriculum-learning/ppo_convlstm.py
+++ curriculum-learning/ppo_convlstm.py
@@ -20,6 +20,85 @@ from boxoban_environment import BoxobanEnvironment
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
+class ConvLSTMCell(nn.Module):
+    def __init__(self, input_channel, num_filter, h_w, kernel_size, stride=1, padding=1):
+        super(ConvLSTMCell, self).__init__()
+        self._conv = nn.Conv2d(in_channels=input_channel + num_filter,
+                               out_channels=num_filter*4,
+                               kernel_size=kernel_size,
+                               stride=stride,
+                               padding=padding)
+        self._state_height, self._state_width = h_w
+        # if using requires_grad flag, torch.save will not save parameters in deed although it may be updated every epoch.
+        # Howerver, if you use declare an optimizer like Adam(model.parameters()),
+        # parameters will not be updated forever.
+        self.Wci = nn.Parameter(torch.zeros(1, num_filter, self._state_height, self._state_width)).to(device)
+        self.Wcf = nn.Parameter(torch.zeros(1, num_filter, self._state_height, self._state_width)).to(device)
+        self.Wco = nn.Parameter(torch.zeros(1, num_filter, self._state_height, self._state_width)).to(device)
+        self._input_channel = input_channel
+        self._num_filter = num_filter
+
+    # inputs and states should not be all none
+    # inputs: S*B*C*H*W
+    def forward(self, inputs, states=None, seq_len=None):
+        seq_len = inputs.size(0) if seq_len is None else seq_len
+
+        if states is None:
+            c = torch.zeros((inputs.size(1), self._num_filter, self._state_height,
+                                  self._state_width), dtype=torch.float).to(device)
+            h = torch.zeros((inputs.size(1), self._num_filter, self._state_height,
+                             self._state_width), dtype=torch.float).to(device)
+        else:
+            h, c = states
+
+        outputs = []
+        for index in range(seq_len):
+            x = inputs[index, ...]
+            cat_x = torch.cat([x, h], dim=1)
+            conv_x = self._conv(cat_x)
+
+            i, f, tmp_c, o = torch.chunk(conv_x, 4, dim=1)
+
+            i = torch.sigmoid(i+self.Wci*c)
+            f = torch.sigmoid(f+self.Wcf*c)
+            c = f*c + i*torch.tanh(tmp_c)
+            o = torch.sigmoid(o+self.Wco*c)
+            h = o*torch.tanh(c)
+            outputs.append(h)
+        return torch.stack(outputs), (h, c)
+
+class ConvLSTM(nn.Module):
+    def __init__(self, input_channels, hidden_channels, input_size, kernel_size, num_layers=1, stride=1, padding=1):
+        super(ConvLSTM, self).__init__()
+
+        input_channels = input_channels if isinstance(input_channels, (list, tuple)) else [input_channels]
+
+        self.layers = nn.ModuleList([
+            ConvLSTMCell(input_channels[i], hidden_channels, input_size, kernel_size, stride, padding)
+            for i in range(num_layers)
+        ])
+
+    # inputs: S*B*C*H*W
+    # hiddens: L*B*C*H*W
+    # cells: L*B*C*H*W
+    # outputs: S*B*C*H*W
+    def forward(self, x, hx):
+        seq_len = x.size(0)
+        hidden, cell = hx
+
+        hiddens = []
+        cells = []
+
+        for i, layer in enumerate(self.layers):
+            x, (hx, cx) = layer(x, (hidden[i], cell[i]), seq_len)
+            hiddens.append(hx)
+            cells.append(cx)
+
+        hiddens = torch.stack(hiddens, dim=0)
+        cells = torch.stack(cells, dim=0)
+
+        return x, (hiddens, cells)
+
 class PPO(nn.Module):
     def __init__(self):
         super(PPO, self).__init__()
@@ -33,25 +112,25 @@ class PPO(nn.Module):
             nn.ReLU()
         )  
 
+        self.convlstm = ConvLSTM(64, 32, (6, 6), 3, num_layers=1)
+
         self.linear = nn.Sequential(
-            nn.Linear(2304, 512),  # 64, 6, 6
+            nn.Linear(1152, 256),  # 32, 6, 6
             nn.ReLU()
         )
-
-        self.lstm = nn.LSTM(512, 256, 1)
 
         self.actor_head = nn.Linear(256, 7)
         self.critic_head = nn.Linear(256, 1)
 
     def forward(self, x, hidden):
         x = self.encoder(x)
-        x = x.view(x.size(0), -1)
 
-        x = self.linear(x)
         x = x.unsqueeze(0)
-
-        x, hidden = self.lstm(x, hidden)
+        x, hidden = self.convlstm(x, hidden)
         x = x.squeeze(0)
+
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
 
         actor = F.softmax(self.actor_head(x), dim=-1)
         critic = self.critic_head(x)
@@ -64,17 +143,13 @@ class PPO(nn.Module):
         x = x.view(-1, 7, 10, 10)
 
         x = self.encoder(x)
-        x = x.view(x.size(0), -1)
-
-        x = self.linear(x)
-        x = x.view(n_envs, n_steps, -1)
-
+        x = x.view(n_envs, n_steps, 64, 6, 6)
 
         done_steps = (dones == True).any(0).nonzero().squeeze(1).tolist()
         done_steps = [-1] + done_steps + ([] if done_steps[-1] == (n_steps - 1) else [n_steps-1])
 
-        hidden = torch.zeros((1, n_envs, 256), device=device)
-        cell = torch.zeros((1, n_envs, 256), device=device)
+        hidden = torch.zeros((1, n_envs, 32, 6, 6), device=device)
+        cell = torch.zeros((1, n_envs, 32, 6, 6), device=device)
         done = torch.full((n_envs, ), True,  device=device)
         rnn_outputs = []
 
@@ -86,14 +161,17 @@ class PPO(nn.Module):
             cell[:, done] = 0
             done = dones[:, end_step]
 
-            rnn_input = x[:, start_step: end_step+1].permute(1,0,2)
-            rnn_output, (hidden, cell) = self.lstm(rnn_input, (hidden, cell))
-            rnn_outputs.append(rnn_output.permute(1, 0, 2))
+            rnn_input = x[:, start_step: end_step+1].permute(1,0,2,3,4)
+            rnn_output, (hidden, cell) = self.convlstm(rnn_input, (hidden, cell))
+            rnn_outputs.append(rnn_output.permute(1,0,2,3,4))
 
         assert sum([x.size(1) for x in rnn_outputs]) == n_steps
 
         x = torch.cat(rnn_outputs, dim=1)
-        x = x.view(n_envs * n_steps, -1)
+        x = x.view(n_envs * n_steps, 32, 6, 6)
+
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
 
         actor = F.softmax(self.actor_head(x), dim=-1)
         critic = self.critic_head(x)
@@ -180,8 +258,8 @@ class ParallelEnv:
         advantages = torch.zeros((self.n_workers, steps), dtype=torch.float32, device=device)
 
         observation = torch.tensor(self.reset(), device=device)
-        hidden = torch.zeros((1, self.n_workers, 256), device=device)
-        cell = torch.zeros((1, self.n_workers, 256), device=device)
+        hidden = torch.zeros((1, self.n_workers, 32, 6, 6), device=device)
+        cell = torch.zeros((1, self.n_workers, 32, 6, 6), device=device)
 
         for t in range(steps):
             with torch.no_grad():
@@ -254,7 +332,7 @@ def compute_loss(model, states, values, actions, log_probabilities, advantages, 
     return loss
 
 def train():
-    learning_rate = 3e-4
+    learning_rate = 1e-4
     gamma = 0.99
     lamda = 0.95
     clip_range = 0.1
