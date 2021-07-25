152dcea0f6582716970b2edfbdd10da4ec90d14a
young <me@xiayang.me>
2021-07-25 23:32:14 +0100

    curriculum learning: 修改GPU集群训练脚本参数

--- curriculum-learning/train.sh
+++ curriculum-learning/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p cs -A cs -q csug
-#SBATCH -c4 --mem=8g
+#SBATCH -c3 --mem=8g
 #SBATCH --gres gpu:1
 module load nvidia/cuda-11.0
 module load nvidia/cudnn-v8.0.180-forcuda11.0
