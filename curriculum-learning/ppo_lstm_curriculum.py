7e9a4d2735d3cee0be01e099324f325202f9941c
young <me@xiayang.me>
2021-08-13 21:39:15 +0100

    curriculum: 初步完成PPO LSTM Curriculum算法

--- curriculum-learning/ppo_lstm_curriculum.py
+++ curriculum-learning/ppo_lstm_curriculum.py
@@ -2,6 +2,8 @@ import math
 from datetime import datetime
 import os
 from distutils.dir_util import copy_tree
+import random
+from collections import deque
 
 import torch
 import torch.nn as nn
@@ -101,9 +103,13 @@ class PPO(nn.Module):
         return actor, critic
 
 
-def collection_worker(queue):
+def collection_worker(queue, curriculum_cursor, curriculum_span):
     while True:
-        queue.put(levelCollection.random())
+        cursor = curriculum_cursor.value
+        revise_range = (16, max(cursor, 16+curriculum_span))
+        learn_range = (cursor, min(cursor+curriculum_span, 250))
+        range = random.choices([revise_range, learn_range], k=1, weights=[0.2, 0.8])[0]
+        queue.put(levelCollection.range(*range))
 
 def worker(master, collection):
     while True:
@@ -127,7 +133,7 @@ def worker(master, collection):
             raise NotImplementedError
 
 class ParallelEnv:
-    def __init__(self, n_workers):
+    def __init__(self, n_workers, curriculum_cursor, curriculum_span):
         self.n_workers = n_workers
         self.workers = []
 
@@ -141,7 +147,7 @@ class ParallelEnv:
             p.start()
             self.workers.append(p)
 
-        p = mp.Process(target=collection_worker, args=(queue, ))
+        p = mp.Process(target=collection_worker, args=(queue, curriculum_cursor, curriculum_span))
         p.daemon = True
         p.start()
         self.collection = p
@@ -265,23 +271,28 @@ def train():
     total_steps = 1e8  # number of timesteps
     n_envs = 32  # number of environment copies simulated in parallel
     n_sample_steps = 128  # number of steps of the environment per sample
-    n_mini_batches = 8  # number of training minibatches per update 
-                                     # For recurrent policies, should be smaller or equal than number of environments run in parallel.
+    n_mini_batches = 8  # number of training minibatches per update
+    # For recurrent policies, should be smaller or equal than number of environments run in parallel.
     n_epochs = 4   # number of training epochs per update
     batch_size = n_envs * n_sample_steps
     n_envs_per_batch = n_envs // n_mini_batches
     n_updates = math.ceil(total_steps / batch_size)
     assert (n_envs % n_mini_batches == 0)
 
+    curriculum_cursor = mp.Value('i', 16)
+    curriculum_rewards = deque(maxlen=50)
+    curriculum_span = 20
+    curriculum_criterion = 9.5
+
     save_path = "./data"
     [os.makedirs(f"{save_path}/{dir}") for dir in ["data", "model", "plot", "runs"] if not os.path.exists(f"{save_path}/{dir}")]
 
-    envs = ParallelEnv(n_envs)
+    envs = ParallelEnv(n_envs, curriculum_cursor, curriculum_span)
     model  = PPO().to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
     step = 0
-    log = pd.DataFrame([], columns=["time", "update", "step", "reward", "average_reward"])
+    log = pd.DataFrame([], columns=["time", "update", "step", "reward", "average_reward", "curriculum_cursor"])
     writer = SummaryWriter()
 
     for update in range(1, n_updates+1):
@@ -311,14 +322,23 @@ def train():
 
         step += batch_size
         reward = total_reward/n_envs
+        curriculum_rewards.append(reward)
         tail_rewards = log["reward"].tail(99)
         average_reward = (tail_rewards.sum() + reward) / (tail_rewards.count() +1)
-        log.at[update] = [datetime.now(), update, step, reward, average_reward]
+        cursor = curriculum_cursor.value
+        log.at[update] = [datetime.now(), update, step, reward, average_reward, cursor]
         
         writer.add_scalar('Reward', reward, update)
         writer.add_scalar('Average reward', average_reward, update)
+        writer.add_scalar('Curriculum cursor', cursor, update)
 
-        print(f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] {update},{step}: {reward:.2f}")
+        print(f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] {update},{step},({cursor},{cursor+curriculum_span}): {reward:.2f}")
+
+        if update % 10 == 0 and len(curriculum_rewards) >= 30:
+            curriculum_average_reward = sum(curriculum_rewards) / len(curriculum_rewards)
+            if curriculum_average_reward >= curriculum_criterion:
+                curriculum_cursor.value = min(cursor+curriculum_span, 250-curriculum_span)
+                curriculum_rewards.clear()
 
         if update % 122 == 0:
             fig = log["average_reward"].plot().get_figure()
