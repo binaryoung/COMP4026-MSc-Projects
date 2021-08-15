486d445fa1ee92e02295b54c590caa06e7e8466b
young <me@xiayang.me>
2021-08-15 22:27:52 +0100

    imitation: 初步完成GAIL算法

--- imitation/gail.py
+++ imitation/gail.py
@@ -10,8 +10,10 @@ import torch.optim as optim
 from torch.distributions import Categorical
 import torch.multiprocessing as mp
 from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import boxoban_level_collection as levelCollection
 from boxoban_environment import BoxobanEnvironment
@@ -20,9 +22,9 @@ from boxoban_environment import BoxobanEnvironment
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class PPO(nn.Module):
+class GAIL(nn.Module):
     def __init__(self):
-        super(PPO, self).__init__()
+        super(GAIL, self).__init__()
 
         self.encoder = nn.Sequential(
             nn.Conv2d(7, 16, kernel_size=3, stride=1),
@@ -38,8 +40,28 @@ class PPO(nn.Module):
             nn.ReLU()
         )
 
-        self.actor_head = nn.Linear(256, 7)
+        self.actor_head = nn.Linear(256, 8)
         self.critic_head = nn.Linear(256, 1)
+
+        self.discriminator_encoder = nn.Sequential(
+            nn.Conv2d(7, 16, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=2, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=2, stride=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(2304, 256),  # 64, 6, 6
+            nn.ReLU()
+        )
+
+        self.discriminator = nn.Sequential(
+            nn.Linear(264, 128),  # 256 + 8
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
+        )
 
     def forward(self, x):
         x = self.encoder(x)
@@ -51,7 +73,80 @@ class PPO(nn.Module):
         critic = self.critic_head(x)
 
         return actor, critic
+    
+    """
+    Expert -> 0
+    Policy -> 1
+    """
+    def discriminate(self, states, actions):
+        states = self.discriminator_encoder(states)
+        actions = F.one_hot(actions, num_classes=8).float()
 
+        x = torch.cat((states, actions), dim=1)
+
+        x = self.discriminator(x)
+
+        return x
+
+    def discriminator_reward(self, states, actions):
+        with torch.no_grad():
+            logits = self.discriminate(states, actions)
+
+            return -F.logsigmoid(logits)
+
+
+class ExpertDataset(Dataset):
+    def __init__(self):
+        self.states = torch.load("samples/states.pt", map_location=device)
+        self.actions = torch.load("samples/actions.pt", map_location=device)
+
+    def __getitem__(self, index):
+        state = self.states[index]
+        action = self.actions[index]
+
+        state = self.to_observation(state)
+
+        return state, action
+
+    def __len__(self):
+        return self.actions.size(0)
+
+    @staticmethod
+    def to_observation(room):
+        wall = torch.zeros((10, 10), dtype=torch.float32, device=device)
+        empty = torch.zeros((10, 10), dtype=torch.float32, device=device)
+        target = torch.zeros((10, 10), dtype=torch.float32, device=device)
+        box = torch.zeros((10, 10), dtype=torch.float32, device=device)
+        box_on_target = torch.zeros((10, 10), dtype=torch.float32, device=device)
+        player = torch.zeros((10, 10), dtype=torch.float32, device=device)
+        player_on_target = torch.zeros((10, 10), dtype=torch.float32, device=device)
+
+        for i, row in enumerate(room):
+            for j, state in enumerate(row):
+                if state == 0:
+                    wall[i, j] = 1
+                elif state == 1:
+                    empty[i, j] = 1
+                elif state == 2:
+                    target[i, j] = 1
+                elif state == 3:
+                    box[i, j] = 1
+                elif state == 4:
+                    box_on_target[i, j] = 1
+                elif state == 5:
+                    player[i, j] = 1
+                elif state == 6:
+                    player_on_target[i, j] = 1
+
+        return torch.stack((
+            wall,
+            empty,
+            target,
+            box,
+            box_on_target,
+            player,
+            player_on_target,
+        ), dim=0)
 
 def collection_worker(queue):
     while True:
@@ -126,6 +221,7 @@ class ParallelEnv:
         log_probabilities = torch.zeros((self.n_workers, steps), dtype=torch.float32, device=device)
 
         rewards = torch.zeros((self.n_workers, steps), dtype=torch.float32, device=device)
+        discriminator_rewards = torch.zeros((self.n_workers, steps), dtype=torch.float32, device=device)
 
         dones = torch.zeros((self.n_workers, steps), dtype=torch.bool, device=device)
 
@@ -144,6 +240,8 @@ class ParallelEnv:
                 actions[:, t] = action
                 log_probabilities[:, t] = p.log_prob(action)
 
+                discriminator_rewards[:, t] = model.discriminator_reward(observation, action).squeeze(-1)
+
             observation, reward, done, info = self.step(action.tolist())
             observation = torch.tensor(observation, device=device)
             rewards[:, t] = torch.tensor(reward, device=device)
@@ -156,7 +254,7 @@ class ParallelEnv:
         for t in reversed(range(steps)):
             mask = 1.0 - dones[:, t].int()
 
-            delta = rewards[:, t] + gamma * last_value * mask - values[:, t]
+            delta = (1 * rewards[:, t] + 0.01 * discriminator_rewards[:, t]) + gamma * last_value * mask - values[:, t]
 
             last_advantage = delta + gamma * lamda * last_advantage * mask
 
@@ -174,8 +272,24 @@ class ParallelEnv:
         targets = advantages + values
 
         total_reward = rewards.sum().item()
+        discriminator_total_reward = discriminator_rewards.sum().item()
 
-        return states, values, actions, log_probabilities, advantages, normalized_advantages, targets, total_reward
+        return states, values, actions, log_probabilities, advantages, normalized_advantages, targets, total_reward, discriminator_total_reward
+
+
+def compute_discriminator_loss(model, expert_states, expert_actions, states, actions):
+    expert_predictions = model.discriminate(expert_states, expert_actions)
+    policy_predictions = model.discriminate(states, actions.long())
+
+    expert_targets = torch.zeros_like(expert_predictions, device=device)
+    policy_targets = torch.ones_like(policy_predictions, device=device)
+
+    expert_loss = F.binary_cross_entropy_with_logits(expert_predictions, expert_targets)
+    policy_loss = F.binary_cross_entropy_with_logits(policy_predictions, policy_targets)
+
+    loss = expert_loss + policy_loss
+
+    return loss
 
 def compute_loss(model, states, values, actions, log_probabilities, advantages, normalized_advantages, targets, clip_range,  value_coefficient, entropy_coefficient):
     pi, v = model(states)
@@ -220,20 +334,52 @@ def train():
     n_updates = math.ceil(total_steps / batch_size)
     assert (batch_size % n_mini_batches == 0)
 
+    n_discriminator_epochs = 5   # number of discriminator training epochs per update
+    discriminator_batch_size = 64  # number of discriminator training batch size per update
+
     save_path = "./data"
-    [os.makedirs(f"{save_path}/{dir}") for dir in ["data", "model", "plot", "runs"] if not os.path.exists(f"{save_path}/{dir}")]
+    [os.makedirs(f"{save_path}/{dir}") for dir in ["data", "model", "plot", "runs", "discriminator", "loss"] if not os.path.exists(f"{save_path}/{dir}")]
 
     envs = ParallelEnv(n_envs)
-    model  = PPO().to(device)
+    model  = GAIL().to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+    expert_dataset = ExpertDataset()
+    expert_dataloader = DataLoader(dataset=expert_dataset, batch_size=discriminator_batch_size, shuffle=True)
+    expert_dataloader = iter(expert_dataloader)
+
     step = 0
-    log = pd.DataFrame([], columns=["time", "update", "step", "reward", "average_reward"])
+    log = pd.DataFrame([], columns=[
+        "time", "update", "step", 
+        "reward", "average_reward", 
+        "discriminator_reward", "discriminator_average_reward",
+        "discriminator_loss", "discriminator_average_loss"
+    ])
     writer = SummaryWriter()
 
     for update in range(1, n_updates+1):
 
-        states, values, actions, log_probabilities, advantages, normalized_advantages, targets, total_reward = envs.sample(model, n_sample_steps, gamma, lamda)
+        states, values, actions, log_probabilities, advantages, normalized_advantages, targets, total_reward, discriminator_total_reward = envs.sample(model, n_sample_steps, gamma, lamda)
+
+        discriminator_loss = 0
+        discriminator_indexes = torch.randperm(batch_size)
+
+        for i in range(0, discriminator_batch_size * n_discriminator_epochs, discriminator_batch_size):
+            discriminator_batch_indexes = discriminator_indexes[i: i + discriminator_batch_size]
+
+            expert_states, expert_actions = next(expert_dataloader)
+
+            loss = compute_discriminator_loss(
+                model,
+                expert_states, expert_actions,
+                states[discriminator_batch_indexes], actions[discriminator_batch_indexes]
+            )
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            discriminator_loss += loss.item()
 
         for _ in range(n_epochs):
 
@@ -259,16 +405,38 @@ def train():
         reward = total_reward/n_envs
         tail_rewards = log["reward"].tail(99)
         average_reward = (tail_rewards.sum() + reward) / (tail_rewards.count() +1)
-        log.at[update] = [datetime.now(), update, step, reward, average_reward]
+        discriminator_reward = discriminator_total_reward/n_envs
+        discriminator_tail_rewards = log["discriminator_reward"].tail(99)
+        discriminator_average_reward = (discriminator_tail_rewards.sum() + discriminator_reward) / (discriminator_tail_rewards.count() +1)
+        discriminator_loss = discriminator_loss/n_discriminator_epochs
+        discriminator_tail_loss = log["discriminator_loss"].tail(99)
+        discriminator_average_loss = (discriminator_tail_loss.sum() + discriminator_loss) / (discriminator_tail_loss.count() +1)
+        log.at[update] = [
+            datetime.now(), update, step, 
+            reward, average_reward, 
+            discriminator_reward, discriminator_average_reward,
+            discriminator_loss, discriminator_average_loss
+        ]
         
         writer.add_scalar('Reward', reward, update)
         writer.add_scalar('Average reward', average_reward, update)
+        writer.add_scalar('Discriminator reward', discriminator_reward, update)
+        writer.add_scalar('Discriminator average reward', discriminator_average_reward, update)
+        writer.add_scalar('Discriminator loss', discriminator_loss, update)
+        writer.add_scalar('Discriminator average loss', discriminator_average_loss, update)
 
-        print(f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] {update},{step}: {reward:.2f}")
+        print(f"[{datetime.now().strftime('%m-%d %H:%M:%S')}] {update},{step}: {reward:.2f}, {discriminator_reward:.3f}, {discriminator_loss:.3f}")
 
         if update % 122 == 0:
             fig = log["average_reward"].plot().get_figure()
             fig.savefig(f"{save_path}/plot/{step}.png")
+            plt.clf()
+            fig = log["discriminator_average_reward"].plot().get_figure()
+            fig.savefig(f"{save_path}/discriminator/{step}.png")
+            plt.clf()
+            fig = log["discriminator_average_loss"].plot().get_figure()
+            fig.savefig(f"{save_path}/loss/{step}.png")
+            plt.clf()
             copy_tree("./runs", f"{save_path}/runs")
 
             torch.save(model.state_dict(), f"{save_path}/model/{step}.pkl")
@@ -277,6 +445,13 @@ def train():
 
     fig = log["average_reward"].plot().get_figure()
     fig.savefig(f"{save_path}/plot/{step}.png")
+    plt.clf()
+    fig = log["discriminator_average_reward"].plot().get_figure()
+    fig.savefig(f"{save_path}/discriminator/{step}.png")
+    plt.clf()
+    fig = log["discriminator_average_loss"].plot().get_figure()
+    fig.savefig(f"{save_path}/loss/{step}.png")
+    plt.clf()
     copy_tree("./runs", f"{save_path}/runs")
     torch.save(model.state_dict(), f"{save_path}/model/{step}.pkl")
     log.to_csv(f"{save_path}/data/{step}.csv", index=False, header=True)
